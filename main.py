8b602b2a03c374b2864348746725fdf21de4a8a7
jddfhfhtrtssgr <123560868+jddfhfhtrtssgr@users.noreply.github.com>
2023-01-27 22:56:19 -0800

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ import requests
 # Making a GET request
 r = requests.get('https://api.github.com/users/naveenkrnl')
   
-# check status code for response received
+# 200 code
 print(r)
 print(r.content)
